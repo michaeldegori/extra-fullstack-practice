4c36530ad0ffd33baf143ea4acaddb750f79dbc7
Michael DeGori <michael.degori@gmail.com>
2020-12-05 17:35:06 -0500

    setup app.js

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const books = require("./books.json");
+const MONGODB_URI = "mongodb://localhost:3000/fullstack-practice";
+const bodyParser = require("body-parser");
+
+app.options('*', cors())
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((self) => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log("Express running...")
+})
