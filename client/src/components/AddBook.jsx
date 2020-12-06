8f291ee4aa3da0dd1b11378331ca9543547fb92c
Michael DeGori <michael.degori@gmail.com>
2020-12-06 08:43:04 -0500

    iteration 7

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -3,7 +3,14 @@ import Axios from 'axios';
 
 const AddBook = () => {
 
-  const [book, setBook] = useState();
+  const [book, setBook] = useState({
+    image_url: "",
+    title: "",
+    description: "",
+    rating: 0,
+    createdAt: Date.now,
+    _v: 0
+  });
 
   const handleChange = (event) => {
     setBook({
@@ -17,21 +24,19 @@ const AddBook = () => {
 
     Axios
       .post('http://localhost:3000/book')
-      .then(res => {
-        console.log("Movie created from React!")
-          .catch(err => console.log(err));
-      })
+      .then(res => console.log("Movie created from React!"))
+      .catch(err => console.log(err))
   }
 
   return (
     <div>
       <form onSubmit={addBook}>
-        <input type="text" name="image-url" placeholder="img url" value={book.image_url} onChange={handleChange} />
+        <input type="text" name="image_url" placeholder="img url" value={book.image_url} onChange={handleChange} />
         <input type="text" name="title" placeholder="title" value={book.title} onChange={handleChange} />
         <input type="text" name="description" placeholder="desc" value={book.description} onChange={handleChange} />
         <input type="number" name="rating" placeholder="rtng" value={book.rating} onChange={handleChange} />
         <input type="date" name="created-at" placeholder="created" value={book.createdAt} onChange={handleChange} />
-        <input type="number" name="version" placeholder="version" value={book._v} onChange={handleChange} />
+        <input type="number" name="_v" placeholder="version" value={book._v} onChange={handleChange} />
         <button type="submit">Submit</button>
       </form>
     </div>
